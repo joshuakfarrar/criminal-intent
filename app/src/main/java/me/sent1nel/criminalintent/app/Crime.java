b0540d1d84fd075f6fa67bfe0c196f1255ceb98d
Joshua K. Farrar <joshua@sent1nel.me>
2014-07-08 10:53:36 -0400

    add date and solved to crime API

--- app/src/main/java/me/sent1nel/criminalintent/app/Crime.java
+++ app/src/main/java/me/sent1nel/criminalintent/app/Crime.java
@@ -1,13 +1,18 @@
 package me.sent1nel.criminalintent.app;
 
+import java.util.Date;
 import java.util.UUID;
 
 public class Crime {
     private UUID id;
     private String title;
+    private Date date;
+    private boolean solved;
+
 
     public Crime() {
         id = UUID.randomUUID();
+        date = new Date();
     }
 
     public UUID getId() {
@@ -20,5 +25,21 @@ public class Crime {
 
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void solve() {
+        this.solved = true;
+    }
+
+    public void unsolve() {
+        this.solved = false;
+    }
+
+    public boolean isSolved() {
+        return solved;
     }
 }
