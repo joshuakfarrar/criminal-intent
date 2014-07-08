c53ae74f297d887e2c0532692c5d23b7033a278d
Joshua K. Farrar <joshua@sent1nel.me>
2014-07-08 10:49:14 -0400

    add textchanged listener for setting crime title

--- app/src/main/java/me/sent1nel/criminalintent/app/CrimeFragment.java
+++ app/src/main/java/me/sent1nel/criminalintent/app/CrimeFragment.java
@@ -5,12 +5,25 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import butterknife.OnTextChanged;
 
 public class CrimeFragment extends Fragment {
+
+    private Crime crime;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        crime = new Crime();
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.crime_fragment, container, false);
         return rootView;
+    }
+
+    @OnTextChanged(R.id.crime_title) void onTextChanged(CharSequence text) {
+        crime.setTitle(text.toString());
     }
 }
